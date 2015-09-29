1e8ad71dfc61a4c12baa000ece1364ac0a11c5f1
George Chung <Gerhut@GMail.com>
2015-09-29 11:46:19 +0800

    fix response

--- media/__main__.py
+++ media/__main__.py
@@ -1,5 +1,5 @@
 from flask import *
-import os, uuid, urlparse
+import os.path, uuid, io, urlparse
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,12 +14,14 @@ def put(filename):
     extension = os.path.splitext(filename)[1]
     filename = uuid.uuid4().hex + extension
     pathname = os.path.join(app.config['MEDIA_ROOT'], filename)
-    file = open(pathname, 'w')
-    file.write(request.stream.read())
-    file.close()
+    with io.open(pathname, 'wb') as file:
+        for chunk in request.stream:
+            file.write(chunk)
     url_root = app.config.get('MEDIA_URL', request.url_root)
     location = urlparse.urljoin(url_root, filename)
-    return '', 201, dict(Location=location)
+    response = make_response('', 201, { "Location": location })
+    del response.headers['Content-Type']
+    return response
 
 if __name__ == '__main__':
     app.run()
